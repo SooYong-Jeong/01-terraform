cc5538b58c9b87f13310101b0594ac26f14057aa
SOOYONG <wjdtndyd7@naver.com>
2023-01-03 15:44:45 +0900

    however end

---
+++ terraform-module/prod/data-stores/mysql/variables.tf
@@ -0,0 +1,20 @@
+variable "db_username" {
+  description = "The username for the database"
+  type        = string
+  sensitive   = true
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "The password for the database"
+  type        = string
+  sensitive   = true
+  default     = "password"
+}
+
+variable "db_name" {
+  description = "The name to use for the database"
+  type        = string
+  sensitive   = true
+  default     = "aws17_example_database_prod"
+}
